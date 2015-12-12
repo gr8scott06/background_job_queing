0889d013cfe65199917efd4879c86d63ce94e381
Scott Stuart <scott@appman.io>
2015-12-11 19:50:47 -0500

    first commit

---
+++ promotion_services.rb
@@ -0,0 +1,29 @@
+class PromotionServices
+
+  include ReferralConstants
+  include ::NewRelic::Agent::MethodTracer
+
+  def initialize(current_user, ip, user_agent)
+    @user = current_user
+    @ip = Rails.env.production? ? ip : "98.209.124.38"
+    @user_agent = CGI.escape(user_agent) #Aarki API
+    @timestamp = Time.now.to_i
+    @promotion_click_public_id = Time.now.to_i.to_s + "-" + SecureRandom.hex(4)
+  end
+
+  def get_all
+    result = [get_fyber,get_aarki]
+    # Parallel.map(["fyber","aarki"], in_threads: 2) do |platform|
+    #   result << offer_platform_parser(platform)
+    # end
+    cpi_offers = result[0][:cpi_offers] + result[1][:cpi_offers]
+    cpe_offers = result[0][:cpe_offers] + result[1][:cpe_offers]
+    pending_offers = result[0][:pending_offers] + result[1][:pending_offers]
+    unique_cpi_offers = parse_unique_offers(cpi_offers)
+    unique_cpe_offers = parse_unique_offers(cpe_offers)
+    return {cpi_offers: unique_cpi_offers, cpe_offers: unique_cpe_offers, pending_offers: pending_offers.first(8), promotion_click_public_id: @promotion_click_public_id}
+  end
+
+  ...
+  
+end
---
+++ promotions_controller.rb
@@ -0,0 +1,25 @@
+class PromotionsController < ApplicationController
+  before_action :mobile_traffic_only
+  before_action :authenticate_user
+  before_action :record_user_activity, only: [:index]
+  include ReferralConstants
+  include ::NewRelic::Agent::MethodTracer
+
+  def index
+    @social_sources = SocialSource.where(active: true).order(:position)
+      .select {|s| !current_user.social_sources.include?(s)}
+  end
+
+  def promotion_service_request
+    promotion_write = Redis.current
+      .setex("promotion_services/#{current_user.id}", 2.minutes, Marshal.dump(PromotionServices.new(current_user, ip, user_agent).get_all))
+    if promotion_write
+      sort_cached_promotions
+    else
+      render layout: false #TODO: better response
+    end
+  end
+
+  ...
+  
+end
